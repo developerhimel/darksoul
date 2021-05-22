0cca3edec27d3cd591c1e795f1126e9eb84e178c
Himel Roy <programmerhimel@gmail.com>
2021-05-22 10:27:12 +0600

    ff-v0.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/teamdh.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ pages/login.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto">
+        <div className="lol">
+          <h1 className="text-center sm:text-left">Hello, teamdh.org</h1>
+        </div>
+      </div>
+    </>
+  )
+}
